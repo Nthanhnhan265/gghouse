35fc37cde5e152716d20c2f10a9323d6dc19cbb6
Nthanhnhan265 <ngthanhnhan265.xb@gmail.com>
2024-11-06 09:38:28 +0700

    update:header, footer

--- wp-content/themes/gghouse/home.php
+++ wp-content/themes/gghouse/home.php
@@ -2,13 +2,13 @@
 
 <div class="blog-posts">
     <?php
-    if ( have_posts() ) :
-        while ( have_posts() ) : the_post();
-            get_template_part( 'template-parts/content', get_post_format() );
+    if (have_posts()) :
+        while (have_posts()) : the_post();
+            get_template_part('template-parts/content', get_post_format());
         endwhile;
         the_posts_navigation();
     else :
-        get_template_part( 'template-parts/content', 'none' );
+        get_template_part('template-parts/content', 'none');
     endif;
     ?>
 </div>
--- wp-content/themes/gghouse/single.php
+++ wp-content/themes/gghouse/single.php
@@ -2,12 +2,12 @@
 
 <div class="single-post">
     <?php
-    if ( have_posts() ) :
-        while ( have_posts() ) : the_post();
-            get_template_part( 'template-parts/content', 'single' );
+    if (have_posts()) :
+        while (have_posts()) : the_post();
+            get_template_part('template-parts/content', 'single');
         endwhile;
     else :
-        get_template_part( 'template-parts/content', 'none' );
+        get_template_part('template-parts/content', 'none');
     endif;
     ?>
 </div>
