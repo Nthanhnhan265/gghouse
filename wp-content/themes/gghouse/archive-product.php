2eab0a0eece01efb474dddf77420fb668b1d2ea9
Nthanhnhan265 <ngthanhnhan265.xb@gmail.com>
2024-11-06 08:19:19 +0700

    update: page

---
+++ wp-content/themes/gghouse/archive-product.php
@@ -0,0 +1,25 @@
+<?php
+if ( ! defined( 'ABSPATH' ) ) {
+    exit; // Tránh truy cập trái phép
+}
+
+get_header( 'shop' );
+?>
+
+<div class="shop-page">
+    <?php
+    if ( have_posts() ) :
+        woocommerce_product_loop_start();
+        while ( have_posts() ) : the_post();
+            wc_get_template_part( 'content', 'product' );
+        endwhile;
+        woocommerce_product_loop_end();
+    else :
+        wc_get_template( 'loop/no-products-found.php' );
+    endif;
+    ?>
+</div>
+
+<?php
+get_footer( 'shop' );
+?>
---
+++ wp-content/themes/gghouse/index.php
@@ -0,0 +1,8 @@
+<?php get_header(); ?>
+
+<main>
+    <h1>Welcome to My Theme</h1>
+    <p>This is a custom WordPress theme.</p>
+</main>
+
+<?php get_footer(); ?>
